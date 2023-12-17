c60e08eb30529ea45b84383a03add1040d311d92
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-17 09:50:29 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
-}
+ const [photoData,setPhotoData] =useState(null);
+ const [loading,setLoading]=useState(false);
+ const [inputNo,setInputNo]=useState('');
+ const fetchData=async()=>{
+    setLoading(true);
+    try{
+        const res=await fetch(`http:jsonplaceholder.typicode.com/photos/${inputNo}`);
+        const json= await res.json();
+        setPhotoData(json);
+    }catch(error){
+        console.error('errorfetching data',error);
+    }finally{
+        setLoading(false);
+    }
+ }
+ useEffect(()=>{
+    if(inputNo){
+        fetchData()
+    }
+ },[inputNo])
+
+ return(
+    <div>
+        <input
+        type='number'
+        value={inputNo}
+        onChange={(e)=>setInputNo(e.target.value)}/>
+        {loading ?<Loader/>:null}
+        {photoData && <PhotoFrame url={photoData.url} title={photoData.title}/>}
+    </div>
+ );
+};
 
 
 export default App;
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import '../styles/App.css';
 
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = ({url,title}) => {
+   <div className='"photoframe'>
+        <img src={url} alt={title}/>
+        <div className='caption'>{title}</div>
+
+   </div>
 }
